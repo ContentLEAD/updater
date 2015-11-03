90416533c3273735bed0c789cc39cd360c1ad8eb
deryk <dking3876@msn.com>
2015-11-03 20:39:23 +0000

    remove test dir

--- testimporter/update.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'classes/connect.php';
-require 'inc/utils.php';
-echo 'this is the update page<br>';
-echo 'Build your auto Update features Here<br>';
-echo 'You will need to include the classes/connection.php with the connection class and the inc/utils.php for functions you will need';
-?>
