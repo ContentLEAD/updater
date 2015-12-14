07d68cb79a54708c26a8caf391d35fb6de3cce9e
dking3876 <dking3876@msn.com>
2015-12-14 16:31:02 -0500

    added missing slash

--- logs.php
+++ logs.php
@@ -6,7 +6,7 @@ if(!is_dir('logs/'.$client)){
     mkdir('logs/'.$client);
     chmod('logs/'.$client, 0777);
 }
-$file = fopen('logs'.$client.'/'.$filename, 'w');
+$file = fopen('logs/'.$client.'/'.$filename, 'w');
 if(fwrite($file, $errors)){
     fclose($file);
     echo 'success';
