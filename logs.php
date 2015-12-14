8763fe3be548fb1293e41e6cf0a8f758b5b392a1
dking3876 <dking3876@msn.com>
2015-12-14 16:04:51 -0500

    create log generating script

---
+++ logs.php
@@ -0,0 +1,14 @@
+<?php 
+$errors = $_POST['errors'];
+$filename = $_POST['filename'];
+$client = $_GET['client'];
+if(!is_dir('logs/'.$client)){
+    mkdir('logs/'.$client);
+}
+$file = fopen('logs'.$client.'/'.$filename, 'w');
+if(fwrite($file, $errors)){
+    fclose($file);
+    echo 'success';
+}else{
+    echo 'failure';
+}
