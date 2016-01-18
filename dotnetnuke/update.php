956ba9d50678ec0b975c9f3ac7daa4544f0da79a
dking3876 <dking3876@msn.com>
2016-01-18 17:11:57 -0500

    modify update page

--- dotnetnuke/update.php
+++ dotnetnuke/update.php
@@ -1,8 +1,8 @@
 <?php
     $results = $con->retrieveData('dotnetnuke', '*', array('ORDER BY'=> array('Vid DESC LIMIT 1')));
-    echo '<pre>';var_dump($results);
-      //$array = new array();
-      //$obj["new_version"] = $results[0]['version'];;
-      //$obj["download_link"] = $results[0]['download_link'];
-       // echo json_encode($obj);
+    
+      $array = new array();
+      $array["new_version"] = $results[0]['version'];;
+      $array["download_link"] = $results[0]['download_link'];
+      echo json_encode($array);
 ?>
