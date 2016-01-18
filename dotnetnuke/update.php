471fbf009c830e50d08feb6e3e84eba5d8808629
dking3876 <dking3876@msn.com>
2016-01-18 17:13:38 -0500

    correct error

--- dotnetnuke/update.php
+++ dotnetnuke/update.php
@@ -1,8 +1,8 @@
 <?php
     $results = $con->retrieveData('dotnetnuke', '*', array('ORDER BY'=> array('Vid DESC LIMIT 1')));
     
-      $array = new array();
-      $array["new_version"] = $results[0]['version'];;
+      $array = array();
+      $array["new_version"] = $results[0]['version'];
       $array["download_link"] = $results[0]['download_link'];
       echo json_encode($array);
 ?>
